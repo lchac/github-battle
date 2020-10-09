6df6c686ebd56fe016b152090fc1fc222e2b158d
lchac <32205749+lchac@users.noreply.github.com>
2020-10-09 19:26:57 -0300

    Added PropTypes

--- app/component/Popular.js
+++ app/component/Popular.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function LanguangesNav({ selected, onUpdateLanguage}) {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -16,6 +17,11 @@ function LanguangesNav({ selected, onUpdateLanguage}) {
             ))}
         </ul>
     )
+}
+
+LanguangesNav.propTypes = {
+    selected: PropTypes.string.isRequired,
+    onUpdateLanguage: PropTypes.func.isRequired
 }
 
 export default class Popular extends React.Component {
