00f13792f03f2416c05fddec9b8557cf838bfb1f
lchac <32205749+lchac@users.noreply.github.com>
2020-10-09 22:08:44 -0300

    Caching repos data

--- app/component/Popular.js
+++ app/component/Popular.js
@@ -31,7 +31,7 @@ export default class Popular extends React.Component {
 
         this.state = {
             selectedLanguage: 'All',
-            repos: null,
+            repos: {},
             error: null
         }
         
@@ -46,26 +46,32 @@ export default class Popular extends React.Component {
   updateLanguage (selectedLanguage) {
     this.setState({
       selectedLanguage,
-      error: null,
-      repos: null
+      error: null
     })
 
-    fetchPopularRepos(selectedLanguage)
-      .then((repos) => this.setState({
-        repos,
-        error: null
-      }))
-      .catch(() => {
-        console.warn('Error fetching repos: ', error)
-
-        this.setState({
-          error: `There was an error fetching the repositories.`
+    if (!this.state.repos[selectedLanguage]) {
+        fetchPopularRepos(selectedLanguage)
+        .then((data) => {
+            this.setState(({repos}) => ({
+                repos:{
+                    ...repos,
+                    [selectedLanguage]: data
+                }
+            }))
         })
-      })
+        .catch(() => {
+          console.warn('Error fetching repos: ', error)
+  
+          this.setState({
+            error: `There was an error fetching the repositories.`
+          })
+        })
+    }
   }
 
     isLoading() {
-        return this.state.repos === null && this.state.error === null;
+        const { selectedLanguage, repos, error } = this.state
+        return !repos[selectedLanguage] && this.state.error === null
     }
 
     render() {
@@ -80,7 +86,7 @@ export default class Popular extends React.Component {
 
                 {this.isLoading() && <p>LOADING</p>}
                 {error && <p>{error}</p>}
-                {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+                {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
 
             </React.Fragment>
         )
