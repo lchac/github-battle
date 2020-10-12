78a08ca4e93ed0b6cbb2454a6074d40829c3e4a8
lchac <32205749+lchac@users.noreply.github.com>
2020-10-12 14:48:07 -0300

    Added Context for changing Theme

--- app/index.js
+++ app/index.js
@@ -3,14 +3,30 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './component/Popular'
 import Battle from './component/Battle'
+import { ThemeProvider } from './context/theme'
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            theme: 'light',
+            toggleTheme: () => {
+                this.setState(({theme}) => ({
+                    theme: theme === 'light' ? 'dark' : 'light'
+                }))
+            }
+        }
+    }
+
     render() {
         return (
-            <div className='container'>
-                <Popular />
-                <Battle />
-            </div>
+            <ThemeProvider value={this.state}>
+                <div className='container'>
+                    {/* <Popular /> */}
+                    <Battle />
+                </div>
+            </ThemeProvider>
         )
     }
 }
