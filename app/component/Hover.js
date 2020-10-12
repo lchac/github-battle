abd002de9617e43a34ed1cdb6f6a4fdba1ed27fe
lchac <32205749+lchac@users.noreply.github.com>
2020-10-12 13:25:06 -0300

    Added component Hover. Render Props

--- app/component/Hover.js
+++ app/component/Hover.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-export default function withHover(Component, propName = 'hovering') {
-    return class WithHover extends React.Component {
+export default class Hover extends React.Component {
         constructor(props) {
             super(props);
             
@@ -26,20 +25,15 @@ export default function withHover(Component, propName = 'hovering') {
         }
 
         render() {
-            const props = {
-                [propName]: this.state.hovering,
-                ...this.props
-            }
 
             return (
                 <div 
                     onMouseOver={this.mouseOver}
                     onMouseOut={this.mouseOut}>
-                        <Component {...props} />
+                        {this.props.children(this.state.hovering)}
                 </div>
             )
         }
     }
-}
 
 
